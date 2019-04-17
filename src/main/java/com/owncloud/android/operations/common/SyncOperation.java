xingxingzhayan/android-1

test CreateFolderOperation without server
Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

src/main/java/com/owncloud/android/operations/common/SyncOperation.java
@@ -24,6 +24,7 @@ import android.content.Context;
 import android.os.Handler;
 
 import com.owncloud.android.datamodel.FileDataStorageManager;
+import com.owncloud.android.datamodel.FileDataStorageManagerInterface;
 import com.owncloud.android.lib.common.OwnCloudClient;
 import com.owncloud.android.lib.common.operations.OnRemoteOperationListener;
 import com.owncloud.android.lib.common.operations.RemoteOperation;
@@ -33,33 +34,33 @@ import com.owncloud.android.lib.common.operations.RemoteOperationResult;
 /**
  * Operation which execution involves both interactions with an ownCloud server and
  * with local data in the device.
- * 
+ *
  * Provides methods to execute the operation both synchronously or asynchronously.
  */
 public abstract class SyncOperation extends RemoteOperation {
-	
+
     //private static final String TAG = SyncOperation.class.getSimpleName();
 
-    private FileDataStorageManager mStorageManager;
-    
-    public FileDataStorageManager getStorageManager() {
+    private FileDataStorageManagerInterface mStorageManager;
+
+    public FileDataStorageManagerInterface getStorageManager() {
         return mStorageManager;
     }
-	
+
 
     /**
      * Synchronously executes the operation on the received ownCloud account.
-     * 
+     *
      * Do not call this method from the main thread.
-     * 
+     *
      * This method should be used whenever an ownCloud account is available, instead of
-     * {@link #execute(OwnCloudClient, com.owncloud.android.datamodel.FileDataStorageManager)}.
-     * 
+     * {@link #execute(OwnCloudClient, com.owncloud.android.datamodel.FileDataStorageManagerInterface)}.
+     *
      * @param storageManager
      * @param context   Android context for the component calling the method.
      * @return          Result of the operation.
      */
-    public RemoteOperationResult execute(FileDataStorageManager storageManager, Context context) {
+    public RemoteOperationResult execute(FileDataStorageManagerInterface storageManager, Context context) {
         if (storageManager == null) {
             throw new IllegalArgumentException("Trying to execute a sync operation with a " +
                     "NULL storage manager");
@@ -71,35 +72,33 @@ public abstract class SyncOperation extends RemoteOperation {
         mStorageManager = storageManager;
         return super.execute(mStorageManager.getAccount(), context);
     }
-    
-	
-	/**
+
+
+    /**
 	 * Synchronously executes the remote operation
-	 * 
+     *
      * Do not call this method from the main thread.
-     * 
+     *
 	 * @param client	Client object to reach an ownCloud server during the execution of the o
      *                  peration.
      * @param storageManager
 	 * @return			Result of the operation.
 	 */
-	public RemoteOperationResult execute(OwnCloudClient client,
-                                         FileDataStorageManager storageManager) {
+    public RemoteOperationResult execute(OwnCloudClient client, FileDataStorageManagerInterface storageManager) {
         if (storageManager == null) {
-            throw new IllegalArgumentException("Trying to execute a sync operation with a " +
-                    "NULL storage manager");
+            throw new IllegalArgumentException("Trying to execute a sync operation with a NULL storage manager");
         }
         mStorageManager = storageManager;
 		return super.execute(client);
 	}
 
-	
+
     /**
      * Asynchronously executes the remote operation
-     * 
+     *
      * This method should be used whenever an ownCloud account is available, instead of
      * {@link #execute(OwnCloudClient)}.
-     * 
+     *
      * @param account           ownCloud account in remote ownCloud server to reach during the
      *                          execution of the operation.
      * @param context           Android context for the component calling the method.
@@ -125,10 +124,10 @@ public abstract class SyncOperation extends RemoteOperation {
     }
     */
 
-    
-	/**
+
+    /**
 	 * Asynchronously executes the remote operation
-	 * 
+     *
 	 * @param client			Client object to reach an ownCloud server during the
      *                          execution of the operation.
 	 * @param listener			Listener to be notified about the execution of the operation.
@@ -146,5 +145,5 @@ public abstract class SyncOperation extends RemoteOperation {
         return super.execute(client, listener, listenerHandler);
 	}
 
-	
+
 }

src/test/java/com/owncloud/android/FileDataStorageManagerLocal.java
@@ -1,0 +1,149 @@
+package com.owncloud.android;
+
+import android.accounts.Account;
+
+import com.owncloud.android.datamodel.FileDataStorageManagerInterface;
+import com.owncloud.android.datamodel.OCFile;
+import com.owncloud.android.lib.resources.shares.OCShare;
+import com.owncloud.android.lib.resources.shares.ShareType;
+import com.owncloud.android.lib.resources.status.OCCapability;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import androidx.annotation.Nullable;
+
+public class FileDataStorageManagerLocal implements FileDataStorageManagerInterface {
+    private Map<String, OCFile> pathToFile = new HashMap<>();
+    private long currentMaxId = 0;
+
+    public FileDataStorageManagerLocal() {
+        // always have root file
+        pathToFile.put("/", createOCFile("/"));
+    }
+
+    @Override
+    public OCFile getFileByPath(String path) {
+        return pathToFile.get(path);
+    }
+
+    @Override
+    public Account getAccount() {
+        throw new IllegalArgumentException("to implement");
+    }
+
+    @Nullable
+    @Override
+    public OCFile getFileById(long id) {
+        for (Map.Entry<String, OCFile> entry : pathToFile.entrySet()) {
+            if (entry.getValue().getFileId() == id) {
+                return entry.getValue();
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public void saveConflict(OCFile file, String etagInConflict) {
+        throw new IllegalArgumentException("to implement");
+    }
+
+    @Override
+    public void deleteFileInMediaScan(String path) {
+        throw new IllegalArgumentException("to implement");
+    }
+
+    @Override
+    public boolean saveFile(OCFile file) {
+        pathToFile.put(file.getRemotePath(), file);
+
+        return true;
+    }
+
+    @Override
+    public boolean fileExists(long id) {
+        throw new IllegalArgumentException("to implement");
+    }
+
+    @Override
+    public List<OCFile> getFolderContent(OCFile f, boolean onlyOnDevice) {
+        throw new IllegalArgumentException("to implement");
+    }
+
+    @Override
+    public void saveFolder(OCFile folder, Collection<OCFile> updatedFiles, Collection<OCFile> filesToRemove) {
+        throw new IllegalArgumentException("to implement");
+    }
+
+    @Override
+    public boolean removeFile(OCFile file, boolean removeDBData, boolean removeLocalCopy) {
+        throw new IllegalArgumentException("to implement");
+    }
+
+    @Override
+    public boolean removeFolder(OCFile folder, boolean removeDBData, boolean removeLocalContent) {
+        throw new IllegalArgumentException("to implement");
+    }
+
+    @Override
+    public void moveLocalFile(OCFile file, String targetPath, String targetParentPath) {
+        throw new IllegalArgumentException("to implement");
+    }
+
+    @Override
+    public boolean saveShare(OCShare share) {
+        throw new IllegalArgumentException("to implement");
+    }
+
+    @Override
+    public List<OCShare> getSharesWithForAFile(String filePath, String accountName) {
+        throw new IllegalArgumentException("to implement");
+    }
+
+    @Override
+    public void removeShare(OCShare share) {
+        throw new IllegalArgumentException("to implement");
+    }
+
+    @Override
+    public void copyLocalFile(OCFile file, String targetPath) {
+        throw new IllegalArgumentException("to implement");
+    }
+
+    @Override
+    public OCShare getFirstShareByPathAndType(String path, ShareType type, String shareWith) {
+        throw new IllegalArgumentException("to implement");
+    }
+
+    @Override
+    public OCCapability saveCapabilities(OCCapability capability) {
+        throw new IllegalArgumentException("to implement");
+    }
+
+    @Override
+    public void saveSharesDB(List<OCShare> shares) {
+        throw new IllegalArgumentException("to implement");
+    }
+
+    @Override
+    public void removeSharesForFile(String remotePath) {
+        throw new IllegalArgumentException("to implement");
+    }
+
+    @Override
+    public OCShare getShareById(long id) {
+        throw new IllegalArgumentException("to implement");
+    }
+
+    public OCFile createOCFile(String path) {
+        OCFile file = new OCFile(path);
+
+        file.setFileId(currentMaxId);
+        currentMaxId++;
+
+        return file;
+    }
+}

itcom/android

src/main/java/com/owncloud/android/operations/common/SyncOperation.java
@@ -24,38 +24,40 @@ import android.content.Context;
 import android.os.Handler;
 
 import com.owncloud.android.datamodel.FileDataStorageManager;
+import com.owncloud.android.datamodel.FileDataStorageManagerInterface;
 import com.owncloud.android.lib.common.OwnCloudClient;
 import com.owncloud.android.lib.common.operations.OnRemoteOperationListener;
 import com.owncloud.android.lib.common.operations.RemoteOperation;
 import com.owncloud.android.lib.common.operations.RemoteOperationResult;
 
-import lombok.Getter;
-
 
 /**
  * Operation which execution involves both interactions with an ownCloud server and
  * with local data in the device.
- * 
+ *
  * Provides methods to execute the operation both synchronously or asynchronously.
  */
 public abstract class SyncOperation extends RemoteOperation {
-    //private static final String TAG = SyncOperation.class.getSimpleName();
+    private FileDataStorageManagerInterface storageManager;
 
-    @Getter private FileDataStorageManager storageManager;
+    public FileDataStorageManagerInterface getStorageManager() {
+        return storageManager;
+    }
+
 
     /**
      * Synchronously executes the operation on the received ownCloud account.
-     * 
+     *
      * Do not call this method from the main thread.
-     * 
+     *
      * This method should be used whenever an ownCloud account is available, instead of
-     * {@link #execute(OwnCloudClient, com.owncloud.android.datamodel.FileDataStorageManager)}.
-     * 
+     * {@link #execute(OwnCloudClient, com.owncloud.android.datamodel.FileDataStorageManagerInterface)}.
+     *
      * @param storageManager
      * @param context   Android context for the component calling the method.
      * @return          Result of the operation.
      */
-    public RemoteOperationResult execute(FileDataStorageManager storageManager, Context context) {
+    public RemoteOperationResult execute(FileDataStorageManagerInterface storageManager, Context context) {
         if (storageManager == null) {
             throw new IllegalArgumentException("Trying to execute a sync operation with a " +
                     "NULL storage manager");
@@ -67,35 +69,33 @@ public abstract class SyncOperation extends RemoteOperation {
         this.storageManager = storageManager;
         return super.execute(this.storageManager.getAccount(), context);
     }
-    
-	
-	/**
+
+
+    /**
 	 * Synchronously executes the remote operation
-	 * 
+     *
      * Do not call this method from the main thread.
-     * 
+     *
 	 * @param client	Client object to reach an ownCloud server during the execution of the o
      *                  peration.
      * @param storageManager
 	 * @return			Result of the operation.
 	 */
-	public RemoteOperationResult execute(OwnCloudClient client,
-                                         FileDataStorageManager storageManager) {
+    public RemoteOperationResult execute(OwnCloudClient client, FileDataStorageManagerInterface storageManager) {
         if (storageManager == null) {
-            throw new IllegalArgumentException("Trying to execute a sync operation with a " +
-                    "NULL storage manager");
+            throw new IllegalArgumentException("Trying to execute a sync operation with a NULL storage manager");
         }
         this.storageManager = storageManager;
 		return super.execute(client);
 	}
 
-	
+
     /**
      * Asynchronously executes the remote operation
-     * 
+     *
      * This method should be used whenever an ownCloud account is available, instead of
      * {@link #execute(OwnCloudClient)}.
-     * 
+     *
      * @param account           ownCloud account in remote ownCloud server to reach during the
      *                          execution of the operation.
      * @param context           Android context for the component calling the method.
@@ -121,10 +121,10 @@ public abstract class SyncOperation extends RemoteOperation {
     }
     */
 
-    
-	/**
+
+    /**
 	 * Asynchronously executes the remote operation
-	 * 
+     *
 	 * @param client			Client object to reach an ownCloud server during the
      *                          execution of the operation.
 	 * @param listener			Listener to be notified about the execution of the operation.

src/test/java/com/owncloud/android/FileDataStorageManagerLocal.java
@@ -1,0 +1,149 @@
+package com.owncloud.android;
+
+import android.accounts.Account;
+
+import com.owncloud.android.datamodel.FileDataStorageManagerInterface;
+import com.owncloud.android.datamodel.OCFile;
+import com.owncloud.android.lib.resources.shares.OCShare;
+import com.owncloud.android.lib.resources.shares.ShareType;
+import com.owncloud.android.lib.resources.status.OCCapability;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import androidx.annotation.Nullable;
+
+public class FileDataStorageManagerLocal implements FileDataStorageManagerInterface {
+    private Map<String, OCFile> pathToFile = new HashMap<>();
+    private long currentMaxId = 0;
+
+    public FileDataStorageManagerLocal() {
+        // always have root file
+        pathToFile.put("/", createOCFile("/"));
+    }
+
+    @Override
+    public OCFile getFileByPath(String path) {
+        return pathToFile.get(path);
+    }
+
+    @Override
+    public Account getAccount() {
+        throw new UnsupportedOperationException("to implement");
+    }
+
+    @Nullable
+    @Override
+    public OCFile getFileById(long id) {
+        for (Map.Entry<String, OCFile> entry : pathToFile.entrySet()) {
+            if (entry.getValue().getFileId() == id) {
+                return entry.getValue();
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public void saveConflict(OCFile file, String etagInConflict) {
+        throw new UnsupportedOperationException("to implement");
+    }
+
+    @Override
+    public void deleteFileInMediaScan(String path) {
+        throw new UnsupportedOperationException("to implement");
+    }
+
+    @Override
+    public boolean saveFile(OCFile file) {
+        pathToFile.put(file.getRemotePath(), file);
+
+        return true;
+    }
+
+    @Override
+    public boolean fileExists(long id) {
+        throw new UnsupportedOperationException("to implement");
+    }
+
+    @Override
+    public List<OCFile> getFolderContent(OCFile f, boolean onlyOnDevice) {
+        throw new UnsupportedOperationException("to implement");
+    }
+
+    @Override
+    public void saveFolder(OCFile folder, Collection<OCFile> updatedFiles, Collection<OCFile> filesToRemove) {
+        throw new UnsupportedOperationException("to implement");
+    }
+
+    @Override
+    public boolean removeFile(OCFile file, boolean removeDBData, boolean removeLocalCopy) {
+        throw new UnsupportedOperationException("to implement");
+    }
+
+    @Override
+    public boolean removeFolder(OCFile folder, boolean removeDBData, boolean removeLocalContent) {
+        throw new UnsupportedOperationException("to implement");
+    }
+
+    @Override
+    public void moveLocalFile(OCFile file, String targetPath, String targetParentPath) {
+        throw new UnsupportedOperationException("to implement");
+    }
+
+    @Override
+    public boolean saveShare(OCShare share) {
+        throw new UnsupportedOperationException("to implement");
+    }
+
+    @Override
+    public List<OCShare> getSharesWithForAFile(String filePath, String accountName) {
+        throw new UnsupportedOperationException("to implement");
+    }
+
+    @Override
+    public void removeShare(OCShare share) {
+        throw new UnsupportedOperationException("to implement");
+    }
+
+    @Override
+    public void copyLocalFile(OCFile file, String targetPath) {
+        throw new UnsupportedOperationException("to implement");
+    }
+
+    @Override
+    public OCShare getFirstShareByPathAndType(String path, ShareType type, String shareWith) {
+        throw new UnsupportedOperationException("to implement");
+    }
+
+    @Override
+    public OCCapability saveCapabilities(OCCapability capability) {
+        throw new UnsupportedOperationException("to implement");
+    }
+
+    @Override
+    public void saveSharesDB(List<OCShare> shares) {
+        throw new UnsupportedOperationException("to implement");
+    }
+
+    @Override
+    public void removeSharesForFile(String remotePath) {
+        throw new UnsupportedOperationException("to implement");
+    }
+
+    @Override
+    public OCShare getShareById(long id) {
+        throw new UnsupportedOperationException("to implement");
+    }
+
+    public OCFile createOCFile(String path) {
+        OCFile file = new OCFile(path);
+
+        file.setFileId(currentMaxId);
+        currentMaxId++;
+
+        return file;
+    }
+}